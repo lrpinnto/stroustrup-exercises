a9baf6b6631b1673f3936b26448ca8901ce0bfc1
Luis Pinto <luisricpinto@gmail.com>
2021-05-16 18:20:44 +0100

    C9DRILL.cpp version 9.4.2

--- CHAPTER 9/C9DRILL.cpp
+++ CHAPTER 9/C9DRILL.cpp
@@ -1,5 +1,5 @@
 #include "../stroustrup/std_lib_facilities.h"
-//-----------------------------------------------------------------------------------------------------------Version $9.4.1
+/*-----------------------------------------------------------------------------------------------------------Version $9.4.1
 struct Date
 {
     int y;
@@ -58,4 +58,52 @@ int main()
     add_day(today,1);
     cout<<today<<'\n';
 }
-//-----------------------------------------------------------------------------------------------------------Version $9.4.1
+-----------------------------------------------------------------------------------------------------------Version $9.4.1*/
+
+//-----------------------------------------------------------------------------------------------------------Version $9.4.2
+struct Date
+{
+    int y,m,d;
+    Date(int y, int m, int d);
+    void add_day(int n);
+};
+
+
+ostream& operator<<(ostream& os, const Date& d)
+{
+    return os << '(' << d.y
+            << ',' << d.m
+            << ',' << d.d << ')';
+}
+
+void Date::add_day(int n)
+{
+    if ((m==2 || m==4 || m==6 || m==9 || m==11) && d+n<=30 && d+n>0)  //Did not implement for different months/years or leap years. Pointless work.
+    {
+        d+=n;
+    }
+    else if (d+n<=31 && d+n>0) d+=n;
+    else return;
+}
+
+Date::Date(int yy, int mm, int dd)          //the book only teaches how to define a constructor a few pages ahead. 
+    :y{yy}, m{mm}, d{dd}
+{
+    if (yy>0 && mm>0 && mm<=12 && dd>0 && dd<=31)
+    {
+        if (!(((yy % 4 == 0) && (yy % 100 != 0)) || (yy % 400 == 0)) && dd==29) error("leap year"); //check if it's a leap year
+    }
+    else error("out of bounds date");
+}
+
+int main()
+{
+    Date today {1978,6,25};
+    cout<<today;
+    Date tomorrow = today;
+    tomorrow.add_day(1);
+    cout<<tomorrow;
+    //Date invalid_test {2004,13,-5};
+    //cout<<invalid_test;
+}
+//-----------------------------------------------------------------------------------------------------------Version $9.4.2
