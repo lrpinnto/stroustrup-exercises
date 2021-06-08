27468a5ddfbd12a35cd5896e6a8d7c54b9120d60
Luis Pinto <luisricpinto@gmail.com>
2021-06-08 20:32:50 +0100

    Solved C9E14.cpp

--- CHAPTER 9/C9E13.cpp
+++ CHAPTER 9/C9E13.cpp
@@ -3,6 +3,7 @@
 //Q: Why would people want to use a Rational class?
 //A: No precision is lost. For example, while handling infinitely long numbers
 
+//----------------------------- rational
 class rational
 {
 private:
@@ -73,10 +74,77 @@ bool operator!=(const rational& a, const rational& b)
 {
     return !(a==b);
 }
+//----------------------------- rational
+
+//----------------------------- money
+class Money
+{
+private:
+    long int cents;
+public:
+    Money(double dollars);
+    long int raw_money() const {return cents;}
+    long int get_dollars() const {return cents*0.01;}
+    string get_cents() const {
+        //return cents%100;
+        int scents {cents%100};
+        if (scents<10)
+        {
+            return "0"+to_string(scents);
+        }
+        else return to_string(scents);
+    }
+};
+
+
+Money::Money(double dollarss)
+{
+    long int dollar = dollarss; //41,556
+    double cent = (dollarss-dollar)*100; //55,6
+    int centz = cent; //55
+    cent = cent-centz; //0,6
+    if (cent<0.5)
+    {
+        cent=floor(cent);
+    }
+    else cent=ceil(cent);
+
+    centz+=cent;
+
+    cents = (dollar*100)+centz;
+}
+
+ostream& operator<<(ostream& os, const Money& d)
+{
+    return os << '$' << d.get_dollars() << '.' << d.get_cents();
+}
+
+Money operator+(const Money& a, const Money& b)
+{
+    double result {};
+    int carryover {0};
+    int cents {stoi(a.get_cents())+stoi(b.get_cents())};
+    if (cents>99)
+    {
+        cents-=100;
+        carryover=1;
+    }
+    result = a.get_dollars()+b.get_dollars()+carryover+cents*0.01;
+    return {result};
+}
+Money operator-(const Money& a, const Money& b)
+{
+    long int dolares {(a.raw_money()-b.raw_money())*0.01}; //easier to work with raw values
+    int centz  {(a.raw_money()-b.raw_money())%100};
+
+    return {dolares+centz*0.01};
+
+}
+
+//----------------------------- money
 
 int main()
 {
-    rational h {1,2};
-    rational b {2*40,4*40};
-    cout<<(h==b);
+    Money teste {1.99};
+    cout<<(teste-teste+teste+teste-teste);
 }
