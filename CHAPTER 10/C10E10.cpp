1d9d030f9e57b8429cf3839c964778d15dc2960a
Luis Pinto <luisricpinto@gmail.com>
2021-07-17 22:21:45 +0100

    Skipped C10E10.cpp

---
+++ CHAPTER 10/C10E10.cpp
@@ -0,0 +1,6 @@
+#include "../stroustrup/std_lib_facilities.h"
+
+//Using calculator from Chapter 8 Ex 01
+//Chapter 10 EX 19
+
+//SKIPPED for now as I was having issues with the debugger accessing the input/output files
