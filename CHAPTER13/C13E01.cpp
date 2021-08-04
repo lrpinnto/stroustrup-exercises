1bab759d565abc9f87851eb37bb5e9559ca41e15
Luis Pinto <luisricpinto@gmail.com>
2021-08-04 23:04:23 +0100

    Working Arc on C13E01.cpp

--- CHAPTER13/C13E01.cpp
+++ CHAPTER13/C13E01.cpp
@@ -1,36 +1,46 @@
 //CHAPTER 13 EX 01
-
+//Started doing the exercise and only then realised this needs to be done for an ellipse instead!
 #include "../sourcesgui/Graph.h"
 #include "../sourcesgui/Simple_window.h"
 #include <stdexcept>
 
-Struct Arc : Shape {
-    Arc(Point p, int rr, double angle) //angle in rads
-
+struct Arc : Circle { //apparently, only inherits from public. Also, declaring objects with the same name as circle seems to overload them
+    Arc(Point p, int rr, double anglee); //angle in degrees because of fl_arc
+    
     void draw_lines() const;
-
-    Point center() const;
-    int radius() const { return r;}
-    void set_radius(int rr)
-    {
-        set_point(0,Point{center().x-rr,center().y-rr});
-
-        r=rr;
-    }
+    void set_angle(int anglee) { angle=anglee; }
 private:
     int r;
+    double angle;
 };
 
-Arc::Arc(Point p, int rr, double angle)
-    :r{rr}
+Arc::Arc(Point p, int rr, double anglee)
+    : r{rr}, angle{anglee}, Circle(p,rr) //When you declare a non-default constructor for a class, the compiler does not generate a default one anymore. So you have to provide your own.
+{                                        //Since circle has no default constructor. It cannot be defined. I assume all inherited functions that are not overloaded work out of the definition of circle
+    add(Point{ p.x - r, p.y - r });      //which should be fine as an arc is essentially a circle  
+}
+void Arc::draw_lines() const  
 {
-    add(Point{p.x-r,p.y-r});
+    if (fill_color().visibility()) {	// fill
+		fl_color(fill_color().as_int());
+		fl_pie(point(0).x,point(0).y,r+r-1,r+r-1,0,angle);
+		fl_color(color().as_int());	// reset color
+	}
+
+	if (color().visibility()) {
+		fl_color(color().as_int());
+		fl_arc(point(0).x,point(0).y,r+r,r+r,0,angle);
+	}
 }
 
 int main()
 try
 {
-
+    Point tl {0,0};
+    Simple_window win {tl,800,1000,"Window"}; 
+    Arc something {Point{300,500},300,180};
+    win.attach(something);
+    win.wait_for_button();
 }
 catch(const std::exception& e)
 {
