7f18399ba8be890c28b54b1c1d58bc2585e9ee3b
Luis Pinto <luisricpinto@gmail.com>
2021-08-12 20:10:42 +0100

    Solved C13E06.cpp

--- CHAPTER13/C13E06.cpp
+++ CHAPTER13/C13E06.cpp
@@ -10,15 +10,31 @@ struct Box_text : Box
 
     void draw_lines() const;
 
-    void set_label(const string& s) { t.set_label(s); }
+    void set_label(const string& s, bool adjust_box) 
+    { 
+        t.set_label(s); 
+        if(adjust_box)
+        {
+            Box::set_height(t.font_size()+4);
+            Box::set_width((t.font_size()/1.7)*t.label().size()); 
+        }
+    }
 	string label() const { return t.label(); }
 
 	void set_font(Graph_lib::Font f) { t.set_font(f); }
 	Graph_lib::Font font() const { return t.font(); }
 
-	void set_font_size(int s) { t.set_font_size(s); }
+	void set_font_size(int s, bool adjust_box) { //if font size is set and adjust_box, change box accordingly
+        t.set_font_size(s);
+        if(adjust_box)
+        {
+            t.move(0,t.font_size()/2);
+            Box::set_height(t.font_size()+4);
+            Box::set_width((t.font_size()/1.7)*t.label().size()); 
+        }
+    }
 	int font_size() const { return t.font_size(); }
-
+    void move(int dx, int dy) {Box::move(dx,dy);t.move(dx,dy);}
     void move_text(int dx, int dy) {t.move(dx,dy);}
 private:
     Text t;
@@ -32,7 +48,7 @@ Box_text::Box_text(Point p, int rr, const string& s)
 {
     t.move(5,t.font_size());            //any magic constants are trial and error in an attempt to fit the Text snuggly within the boxes walls
     Box::set_height(t.font_size()+4);
-    Box::set_width((t.font_size()/1.9)*s.size());
+    Box::set_width((t.font_size()/1.7)*s.size());
 }
 
 void Box_text::draw_lines() const
@@ -41,30 +57,104 @@ void Box_text::draw_lines() const
     t.draw_lines();
 }
 
+Point n(Box_text& smth)
+{
+    return {smth.point(0).x+ smth.get_width()/2,smth.point(0).y};
+}
+Point s(Box_text& smth)
+{
+    return {smth.point(0).x+ smth.get_width()/2,smth.point(0).y+smth.get_height()};
+}
+
 int main()
 try
 {
+    const int win_x  {1600};
+    const int win_y  {900};
     Point tl {100,100};
-    Simple_window win {tl,1600,900,"Window"}; 
-    Box_text bb {{300,300},50,150,10,"something something"};
+    Simple_window win {tl,win_x,win_y,"Window"}; 
+    Box_text bb {{300,300},50,150,10,"Custom box with text"};
+    Box_text autobox {{500,500},5,"Automated text box"};
+    Mark m {{300,300},'X'};
+    Mark h {{500,500},'X'};
+    Mark z {n(autobox),'X'};
+    Mark w {s(autobox),'X'};
     win.attach(bb);
-
+    win.attach(m);
+    win.set_label("a");
     win.wait_for_button();
     bb.set_font(Graph_lib::Font::courier_bold);
     bb.set_fill_color(Color::dark_green);
-    win.set_label("a");
+    win.set_label("Set fill Color, set font");
     win.wait_for_button();
-    Mark m {{500,500},'X'};
-    Box_text sna {{500,500},5,"sjadj jksd jkshdfsgdfhgshfjfghjgshfjgfdghjsdhfgshjgfghfjghj"};
-    
-    win.attach(sna);
+    win.attach(autobox);
+    win.attach(z);
+    win.attach(w);
+    win.attach(h);
+    win.set_label("Auto text box");
     win.wait_for_button();
-    win.attach(m);
+    autobox.set_font_size(30,true); 
+    autobox.move(100,100);
+    win.set_label("Font size and move");
     win.wait_for_button();
-    sna.set_font_size(30);  //box cant keep up with font changes yet
-    sna.set_font(Graph_lib::Font::courier_bold);
+    autobox.set_font(Graph_lib::Font::courier_bold);
+    autobox.set_label("long text TEST TEST TEST TEST TEST TEST TEST",true);
+    autobox.set_color(Color::blue);
+    win.set_label("Change font, change color, change text");
     win.wait_for_button();
-    
+    win.detach(autobox);
+    win.detach(m);
+    win.detach(bb);
+    win.detach(h);
+    win.detach(z);
+    win.detach(w);
+    win.set_label("clear");
+    win.wait_for_button();
+
+    Vector_ref<Box_text> top_row;
+    top_row.push_back(new Box_text{{win_x*2/6,win_y*1/5},5,"Window"});
+    top_row.push_back(new Box_text{{win_x*3/6,win_y*1/5},5,"Line_style"});
+    top_row.push_back(new Box_text{{win_x*4/6,win_y*1/5},5,"Color"});
+    Vector_ref<Box_text> middle_row;
+    middle_row.push_back(new Box_text{{win_x*2/6,win_y*2/5},5,"Simple_window"});
+    middle_row.push_back(new Box_text{{win_x*3/6,win_y*2/5},5,"Shape"});
+    middle_row.push_back(new Box_text{{win_x*4/6,win_y*2/5},5,"Point"});
+    Vector_ref<Box_text> bottom_row;
+    bottom_row.push_back(new Box_text{{win_x*0/7,win_y*3/5},5,"Line"});
+    bottom_row.push_back(new Box_text{{win_x*1/7,win_y*3/5},5,"Lines"});
+    bottom_row.push_back(new Box_text{{win_x*2/7,win_y*3/5},5,"Polygon"});
+    bottom_row.push_back(new Box_text{{win_x*3/7,win_y*3/5},5,"Axis"});
+    bottom_row.push_back(new Box_text{{win_x*4/7,win_y*3/5},5,"Rectangle"});
+    bottom_row.push_back(new Box_text{{win_x*5/7,win_y*3/5},5,"Text"});
+    bottom_row.push_back(new Box_text{{win_x*6/7,win_y*3/5},5,"Image"});
+
+    Vector_ref<Arrow> arrows;
+    for (int i = 1; i <= top_row.size(); i++)
+    {
+        top_row[top_row.size()-i].set_fill_color(Color::green);
+        top_row[top_row.size()-i].set_font_size(20,true);
+        top_row[top_row.size()-i].set_font(Graph_lib::Font::times_bold);
+        middle_row[middle_row.size()-i].set_fill_color(Color::green);
+        middle_row[middle_row.size()-i].set_font_size(20,true);
+        middle_row[middle_row.size()-i].set_font(Graph_lib::Font::times_bold);
+        win.attach(top_row[top_row.size()-i]);
+        win.attach(middle_row[top_row.size()-i]);
+    }
+    for (int i = 1; i <= bottom_row.size(); i++)
+    {
+        arrows.push_back(new Arrow{n(bottom_row[bottom_row.size()-i]),s(middle_row[1])});
+        bottom_row[bottom_row.size()-i].set_fill_color(Color::green);
+        bottom_row[bottom_row.size()-i].set_font_size(20,true);
+        bottom_row[bottom_row.size()-i].set_font(Graph_lib::Font::times_bold);
+        arrows[arrows.size()-1].set_fill_color(Color::black);
+        win.attach(bottom_row[bottom_row.size()-i]);
+        win.attach(arrows[arrows.size()-1]);
+    }
+    Arrow lonelyarrow {n(middle_row[0]),s(top_row[0])};
+    lonelyarrow.set_fill_color(Color::black);
+    win.attach(lonelyarrow);
+    win.label("Chapter 12.7 Illustration");
+    win.wait_for_button();
 }
 catch(const std::exception& e)
 {
