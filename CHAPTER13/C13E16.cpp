ab2b0cecb5d58209e8ca9df4b90da1a3233098c1
lrpinnto <luisricpinto@gmail.com>
2021-08-16 11:48:47 +0100

    Solved C13E16.cpp and E17

---
+++ CHAPTER13/C13E16.cpp
@@ -0,0 +1,45 @@
+//CHAPTER 13 EX 16 & 17
+
+#include "./sources.h"
+#include <stdexcept>
+
+int main()
+try
+{
+    int win_x {1600};
+    int win_y {900};
+    Point tl {100,100};
+    Simple_window win {tl,win_x,win_y,"Window"}; 
+    cout<<"Input the desired radius:";
+    int r;
+    cin>>r;
+    int horizontal_distance_2_center {round(r*sin(45))};
+    int half_length_vertical_side {round(r*cos(45))};
+    Vector_ref<Regular_hexagon> rh;
+    for (int j = 0; j*r < win_y; j++)
+    {
+        for (int i = 0; i*horizontal_distance_2_center < win_x; i++)
+        {
+            if(j%2==0)
+            {
+                rh.push_back(new Regular_hexagon{Point{horizontal_distance_2_center*2*i,j*(r+half_length_vertical_side)},r});
+            }
+            else
+            {
+                rh.push_back(new Regular_hexagon{Point{horizontal_distance_2_center*2*i+horizontal_distance_2_center,j*(r+half_length_vertical_side)},r});
+            }
+            rh[rh.size()-1].set_fill_color(rand(0,255));
+            win.attach(rh[rh.size()-1]);
+        }
+    }
+    win.wait_for_button();
+}
+catch(const std::exception& e)
+{
+    std::cerr << e.what() << '\n';
+}
+catch(...)
+{
+    // some more error reporting
+    return 2;
+}
