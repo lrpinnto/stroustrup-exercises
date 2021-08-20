774aa04ffd2798b480a790ce7686287a0bf388bc
lrpinnto <luisricpinto@gmail.com>
2021-08-20 16:25:17 +0100

    Added C14E02.cpp

---
+++ CHAPTER14/C14E02.cpp
@@ -0,0 +1,28 @@
+//CHAPTER 14 EX 02
+
+#include "./sources.h"
+#include <stdexcept>
+
+int main()
+try
+{
+    int win_x {1600};
+    int win_y {900};
+    Point tl {100,100};
+    Simple_window win {tl,win_x,win_y,"Window"}; 
+
+    Shape& ref_shape;
+    Circle c {{100,100},100};
+    ref_shape=c;
+
+    win.wait_for_button();
+}
+catch(const std::exception& e)
+{
+    std::cerr << e.what() << '\n';
+}
+catch(...)
+{
+    // some more error reporting
+    return 2;
+}
