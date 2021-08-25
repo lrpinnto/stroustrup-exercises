7c1e1cd0b8da716548fe103fb1a7edda184155ba
lrpinnto <luisricpinto@gmail.com>
2021-08-25 11:49:31 +0100

    C14E15.cpp Updated iterators and overloaded <<

--- CHAPTER14/C14E15.cpp
+++ CHAPTER14/C14E15.cpp
@@ -16,17 +16,46 @@ public:
 class Vector_iterator : public Iterator
 {
 private:
-    /* data */
+    vector<double> v;
+    int index;
 public:
-    Vector_iterator(const vector<double>& v){}
-    double* next() override {}
+    Vector_iterator(const vector<double>& vv)
+        : v{vv}, index{0} {}
+    double* next() override {return &v[index++];} //it will return the x's value before incrementing it and after that the value of x will be + 1
 };
 
 class List_iterator : public Iterator
 {
 private:
-    /* data */
+    list<double> l;
+    list<double>::iterator pointer;  //yet to be introduced in the book
 public:
-    List_iterator(const list<double>& l){}
-    double* next() override {}
+    List_iterator(const list<double>& ll)
+        : l{ll}, pointer{l.begin()} {}
+    double* next() override //pointers and lists not introduced yet in this chapter. Need to fully understand this code
+    {
+        double* p;
+//if (l.size() && l.size() > distance(lis.begin(), pointer)) {
+        p = &*pointer;
+        pointer++;
+        return p;
+    }
 };
+
+ostream& operator<<(ostream& os, Iterator& ite)
+{
+    double* pointer;
+    pointer = ite.next();
+    double &value = *pointer;
+    return os<<value<<'\n';
+}
+
+int main()
+{
+    vector<double> abc {1,2,3};
+    Vector_iterator vi {abc};
+    cout<<vi<<vi<<vi<<vi; //last vi returns 0. Needs if() check to avoid going out of bounds;
+    list<double> cba {1,2,3};
+    List_iterator li {cba};
+    cout<<li<<li<<li<<li; //last vi returns gibberish. Needs if() check to avoid going out of bounds;
+}
