fc3ee2f00c1c2a0e3fe43285686d05c1b9a042c0
Luis Pinto <luisricpinto@gmail.com>
2021-09-07 22:52:08 +0100

    Added header files to CHAPTER15, C15DRILL.cpp bugfixing

--- CHAPTER15/C15DRILL.cpp
+++ CHAPTER15/C15DRILL.cpp
@@ -9,27 +9,39 @@ try
     int win_x {600};
     int win_y {600};
     Point tl {100,100};
-    Window win {tl,win_x,win_y,"Function graphs"}; 
+    Graph_lib::Window win {tl,win_x,win_y,"Function graphs"}; 
     Axis x {Axis::Orientation::x,{100,300},400,20,"1==20 pixels"};
-    Axis y {Axis::Orientation::y,{300,100},400,20,"1==20 pixels"};
-    y.notches.set_color(Color::red);
-    x.notches.set_color(Color::red);
-
-    Function one { [](double x)->double {return 1;},-10,11,{300,300},400};
+    Axis y {Axis::Orientation::y,{300,500},400,20,"1==20 pixels"};
+    x.set_color(Color::red);
+    y.set_color(Color::red);
+    x.label.set_color(Color::black);
+    x.label.move(-30,0);
+    y.label.set_color(Color::black);
+    win.attach(x);
+    win.attach(y);
+    /*
+    Function one { [](double x)->double {return 1;},-10,11,{300,300},400}; 
     win.attach(one);
-    Function one1 { [](double x)->double {return 1;},-10,11,{300,300},400,20,20};
-    win.attach(one1);
+    */
+    Function one { [](double x)->double {return 1;},-10,11,{300,300},400,20,20};
+    win.attach(one);
+
     Function slope { [](double x)->double {return x/2;}, -10, 11, {300,300},400,20,20};
     win.attach(slope);
-    Text slope_text {{100,500},"x/2"};
+    Text slope_text {{300-(10*20),(300-(10*20))/2},"x/2"};
     win.attach(slope_text);
+
     Function square { [](double x)->double {return x*x;}, -10, 11, {300,300},400,20,20 };
     win.attach(square);
+
     Function cosine { cos, -10, 11, {300,300},400,20,20 };
     cosine.set_color(Color::blue);
     win.attach(cosine);
+
     Function sloping_cos { [](double x)->double {x/2 + cos(x);}, -10, 11, {300,300},400,20,20 };
     win.attach(sloping_cos);
+
+    gui_main();
 }
 catch(const std::exception& e)
 {
---
+++ CHAPTER15/sources.cpp
@@ -0,0 +1,3 @@
+//g++ -w -Wall -std=c++11 ../sourcesgui/Graph.cpp ../sourcesgui/Window.cpp ../sourcesgui/GUI.cpp '../sourcesgui/Simple_window.cpp' ../CHAPTER13/sources.cpp ../CHAPTER14/sources.cpp sources.cpp  C15EXX.cpp `fltk-config --ldflags --use-images` -o C15EXX
+
+#include "./sources.h"
