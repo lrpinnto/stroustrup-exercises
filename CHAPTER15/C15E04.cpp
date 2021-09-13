79371c4b76e022dc883b63c90527c0dd12f9ea13
Luis Pinto <luisricpinto@gmail.com>
2021-09-13 23:05:13 +0100

    Solved C15E04.cpp

--- CHAPTER15/C15E04.cpp
+++ CHAPTER15/C15E04.cpp
@@ -3,15 +3,33 @@
 #include "./sources.h"
 #include <stdexcept>
 
+//Functions don't seem to be properly aligned with the notches but that might just be some quirk with how FLTK works
+
 int main()
 try
 {
-    int win_x {600};
-    int win_y {600};
+    constexpr int xmax {600};
+    constexpr int ymax {600};
+
+    constexpr int x_orig {xmax/2};
+    constexpr int y_orig {ymax/2};
+    const Point orig {x_orig,y_orig};
+
+    constexpr int r_min {-10};
+    constexpr int r_max {11};
+
+    constexpr int n_points {400};
+
+    constexpr int x_scale {20};
+    constexpr int y_scale {20};
+
+    constexpr int xlength {xmax-200}; //400
+    constexpr int ylength {ymax-200}; //400
+
     Point tl {100,100};
-    Graph_lib::Window win {tl,win_x,win_y,"Function graphs"}; 
-    Axis x {Axis::Orientation::x,{100,300},400,20,"1==20 pixels"};
-    Axis y {Axis::Orientation::y,{300,500},400,20,"1==20 pixels"};
+    Graph_lib::Window win {tl,xmax,ymax,"Function graphs"}; 
+    Axis x {Axis::Orientation::x,{orig.x-(xlength/2),orig.y},xlength,xlength/x_scale,"1==20 pixels"};
+    Axis y {Axis::Orientation::y,{orig.x,orig.y+(ylength/2)},ylength,ylength/y_scale,"1==20 pixels"};
     x.set_color(Color::red);
     y.set_color(Color::red);
     x.label.set_color(Color::black);
@@ -21,27 +39,31 @@ try
     win.attach(y);
 
     //functions
-    Function sine { sin, -10, 11, {300,300},400,20,20 };
-    cosine.set_color(Color::blue);
+    Function sine { sin, r_min,r_max,orig,n_points,x_scale,y_scale};
+    sine.set_color(Color::blue);
     win.attach(sine);
 
-    Function cosine { cos, -10, 11, {300,300},400,20,20 };
+    Function cosine { cos, r_min,r_max,orig,n_points,x_scale,y_scale};
     cosine.set_color(Color::green);
     win.attach(cosine);
 
-    Function sinecosine { [](double x)->double {return sin(x)+cos(x)} , -10, 11, {300,300},400,20,20 };
-    cosine.set_color(Color::dark_yellow);
+    Function sinecosine { [](double x)->double {return sin(x)+cos(x);},r_min,r_max,orig,n_points,x_scale,y_scale};
+    sinecosine.set_color(Color::dark_yellow);
     win.attach(sinecosine);
 
-    Function sinecosinesquare { [](double x)->double {return sin(x)*sin(x)+cos(x)*cos(x)} , -10, 11, {300,300},400,20,20 };
-    cosine.set_color(Color::dark_cyan);
+    Function sinecosinesquare { [](double x)->double {return sin(x)*sin(x)+cos(x)*cos(x);}, r_min,r_max,orig,n_points,x_scale,y_scale};
+    sinecosinesquare.set_color(Color::dark_cyan);
     win.attach(sinecosinesquare);
 
     //labels
-    Text sinet {{300-(10*20),y_point},"sin()"};
-    Text cosinet {{300-(10*20),y_point},"cos()"};
-    Text sinecosinet {{300-(10*20),y_point},"sin()+cos()"};
-    Text sinecosinesquaret {{300-(10*20),y_point},"sin()*sin()+cos()*cos()"};
+    Text sinet {{orig.x-(r_min*x_scale),orig.y-(sin(r_min))*y_scale-1},"sin()"};
+    sinet.set_color(Color::blue);
+    Text cosinet {{orig.x-(r_min*x_scale),orig.y-(cos(r_min))*y_scale-1},"cos()"};
+    cosinet.set_color(Color::green);
+    Text sinecosinet {{orig.x-(r_min*x_scale),orig.y-(sin(r_min)+cos(r_min))*y_scale-1},"sin()+cos()"};
+    sinecosinet.set_color(Color::dark_yellow);
+    Text sinecosinesquaret {{orig.x-(r_min*x_scale),orig.y-(sin(r_min)*sin(r_min)+cos(r_min)*cos(r_min))*y_scale-1},"sin()*sin()+cos()*cos()"};
+    sinecosinesquaret.set_color(Color::dark_cyan);
     win.attach(sinet);
     win.attach(cosinet);
     win.attach(sinecosinet);
