44add48b95ff4d6736aad38e491c9b0142a54a0c
Luis Pinto <luisricpinto@gmail.com>
2021-09-13 18:31:09 +0100

    Added missing files

---
+++ CHAPTER 6/C6E5.cpp
@@ -0,0 +1,26 @@
+//CHAPTER 6 EXERCISE 5
+
+//double check if solution is correct
+
+/*
+GRAMMAR
+=======
+Sentence:
+    Noun Verb
+    Article Noun Verb
+    Sentence Conjunction Sentence
+Conjunction:
+    "and"
+    "or"
+    "but"
+Noun:
+    "birds"
+    "fish"
+    "C++"
+Verb:
+    "rules"
+    "fly"
+    "swim"
+Article:
+    "the"
+*/
