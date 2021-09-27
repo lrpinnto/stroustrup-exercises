00c664c01e53e71eb791d5076f25f88b7bf7dbb9
Luis Pinto <luisricpinto@gmail.com>
2021-09-27 20:03:39 +0100

    Solved C14E17.cpp

--- CHAPTER14/C14E17.cpp
+++ CHAPTER14/C14E17.cpp
@@ -12,22 +12,80 @@ int main()
     Point tl {100,100};
     Simple_window win {tl,win_x,win_y,"Window"}; 
 
-    Text title {{900,10},"Exceptions"};
-    Text sub_title {{900,100},"C++ Exception Classes"};
-    Text topic {{100,200},"#include <stdexcept>"};
+    Text title {{700,20},"Exceptions"};
+    title.set_font_size(20);
+    title.set_font(Graph_lib::Font::courier_bold);
+    Text sub_title {{700,50},"C++ Exception Classes"};
+    sub_title.set_font_size(20);
+    sub_title.set_font(Graph_lib::Font::courier_bold);
+    Text topic {{100,100},"#include <stdexcept>"};
+    topic.set_font_size(20);
+    topic.set_font(Graph_lib::Font::courier_bold);
+    win.attach(title);
+    win.attach(sub_title);
+    win.attach(topic);
+
+    Vector_ref<Box_text>vr;
+    Vector_ref<Arrow>vr2;
     //--
-    Box_text one {{900,300},5,"exception"};
+    vr.push_back(new Box_text{{900,300},5,"exception"}); 
+    vr[0].set_fill_color(Color::magenta);
+    vr[0].set_font(Graph_lib::Font::helvetica_bold_italic);
     //--
-    Box_text two {{100,400},5,"bad_alloc"};
-    Box_text three {{500,400},5,"logic_error"};
-    Box_text four {{900,400},5,"runtime_error"};
-    Box_text five {{1300,400},5,"bad_cast"};
+    vr.push_back(new Box_text{{100,400},5,"bad_alloc"});
+    vr[1].set_fill_color(Color::blue);
+    vr[1].set_font(Graph_lib::Font::helvetica_bold_italic);
+    vr.push_back(new Box_text{{500,400},5,"logic_error"});
+    vr[2].set_fill_color(Color::green);
+    vr[2].set_font(Graph_lib::Font::helvetica_bold_italic);
+    vr.push_back(new Box_text{{900,400},5,"runtime_error"});
+    vr[3].set_fill_color(Color::blue);
+    vr[3].set_font(Graph_lib::Font::helvetica_bold_italic);
+    vr.push_back(new Box_text{{1300,400},5,"bad_cast"});
+    vr[4].set_fill_color(Color::red);
+    vr[4].set_font(Graph_lib::Font::helvetica_bold_italic);
+
+    vr2.push_back(new Arrow{n(vr[1]),s(vr[0])});
+    vr2.push_back(new Arrow{n(vr[2]),s(vr[0])});
+    vr2.push_back(new Arrow{n(vr[3]),s(vr[0])});
+    vr2.push_back(new Arrow{n(vr[4]),s(vr[0])});
     //--
-    Box_text six {{100,500},5,"length_error"};
-    Box_text seven {{100,600},5,"domain_error"};
-    Box_text eight {{100,700},5,"out_of_range"};
-    Box_text nine {{100,800},5,"invalid_argument"};
-    Box_text ten {{1100,500},5,"range_error"};
-    Box_text eleven {{1100,600},5,"overflow_error"};
-    Box_text twelve {{1100,700},5,"underflow_error"};
+    vr.push_back(new Box_text{{100,500},5,"length_error"});
+    vr[5].set_fill_color(Color::green);
+    vr[5].set_font(Graph_lib::Font::helvetica_bold_italic);
+    vr.push_back(new Box_text{{100,600},5,"domain_error"});
+    vr[6].set_fill_color(Color::green);
+    vr[6].set_font(Graph_lib::Font::helvetica_bold_italic);
+    vr.push_back(new Box_text{{100,700},5,"out_of_range"});
+    vr[7].set_fill_color(Color::green);
+    vr[7].set_font(Graph_lib::Font::helvetica_bold_italic);
+    vr.push_back(new Box_text{{100,800},5,"invalid_argument"});
+    vr[8].set_fill_color(Color::green);
+    vr[8].set_font(Graph_lib::Font::helvetica_bold_italic);
+    vr.push_back(new Box_text{{1100,500},5,"range_error"});
+    vr[9].set_fill_color(Color::blue);
+    vr[9].set_font(Graph_lib::Font::helvetica_bold_italic);
+    vr.push_back(new Box_text{{1100,600},5,"overflow_error"});
+    vr[10].set_fill_color(Color::blue);
+    vr[10].set_font(Graph_lib::Font::helvetica_bold_italic);
+    vr.push_back(new Box_text{{1100,700},5,"underflow_error"});
+    vr[11].set_fill_color(Color::blue);
+    vr[11].set_font(Graph_lib::Font::helvetica_bold_italic);
+
+    vr2.push_back(new Arrow{n(vr[5]),s(vr[2])});
+    vr2.push_back(new Arrow{n(vr[6]),s(vr[2])});
+    vr2.push_back(new Arrow{n(vr[7]),s(vr[2])});
+    vr2.push_back(new Arrow{n(vr[8]),s(vr[2])});
+
+    vr2.push_back(new Arrow{n(vr[9]),s(vr[3])});
+    vr2.push_back(new Arrow{n(vr[10]),s(vr[3])});
+    vr2.push_back(new Arrow{n(vr[11]),s(vr[3])});
+
+    for (int i = 0; i < vr.size(); i++)
+        win.attach(vr[i]);
+
+    for (int i = 0; i < vr2.size(); i++)
+        win.attach(vr2[i]);
+    
+    win.wait_for_button();
 }
