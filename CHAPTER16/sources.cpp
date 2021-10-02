d20d8be6fe99c9b3526e50b61878c74801e1ed79
Luis Pinto <luisricpinto@gmail.com>
2021-10-02 22:06:27 +0100

    Solved C16E02.cpp

--- CHAPTER16/sources.cpp
+++ CHAPTER16/sources.cpp
@@ -53,3 +53,187 @@ void My_window::quit()
 }
 //------------------------------------------------------------------------------
 //EX 01--
+
+//EX 02
+Checkerboard_window::Checkerboard_window(Point xy, int w, int h, const string& title)
+    :   My_window{xy,w,h,title},
+        button1{Point{x_max()/2-75,y_max()/2-75}, 50, 50, "1", cb_1},
+        button2{Point{x_max()/2-25,y_max()/2-75}, 50, 50, "2", cb_2},
+        button3{Point{x_max()/2+25,y_max()/2-75}, 50, 50, "3", cb_3},
+        button4{Point{x_max()/2+75,y_max()/2-75}, 50, 50, "4", cb_4},
+        button5{Point{x_max()/2-75,y_max()/2-25}, 50, 50, "5", cb_5},
+        button6{Point{x_max()/2-25,y_max()/2-25}, 50, 50, "6", cb_6},
+        button7{Point{x_max()/2+25,y_max()/2-25}, 50, 50, "7", cb_7},
+        button8{Point{x_max()/2+75,y_max()/2-25}, 50, 50, "8", cb_8},
+        button9{Point{x_max()/2-75,y_max()/2+25}, 50, 50, "9", cb_9},
+        button10{Point{x_max()/2-25,y_max()/2+25}, 50, 50, "10", cb_10},
+        button11{Point{x_max()/2+25,y_max()/2+25}, 50, 50, "11", cb_11},
+        button12{Point{x_max()/2+75,y_max()/2+25}, 50, 50, "12", cb_12},
+        button13{Point{x_max()/2-75,y_max()/2+75}, 50, 50, "13", cb_13},
+        button14{Point{x_max()/2-25,y_max()/2+75}, 50, 50, "14", cb_14},
+        button15{Point{x_max()/2+25,y_max()/2+75}, 50, 50, "15", cb_15},
+        button16{Point{x_max()/2+75,y_max()/2+75}, 50, 50, "16", cb_16},
+        random_box{Point{0,0},100,20,"random output: "}
+{
+    attach(button1);
+    attach(button2);
+    attach(button3);
+    attach(button4);
+    attach(button5);
+    attach(button6);
+    attach(button7);
+    attach(button8);
+    attach(button9);
+    attach(button10);
+    attach(button11);
+    attach(button12);
+    attach(button13);
+    attach(button14);
+    attach(button15);
+    attach(button16);
+    attach(random_box);
+}
+
+void Checkerboard_window::cb_1(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).one();    
+}
+void Checkerboard_window::cb_2(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).two();    
+}
+void Checkerboard_window::cb_3(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).three();    
+}
+void Checkerboard_window::cb_4(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).four();    
+}
+void Checkerboard_window::cb_5(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).five();    
+}
+void Checkerboard_window::cb_6(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).six();    
+}
+void Checkerboard_window::cb_7(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).seven();    
+}
+void Checkerboard_window::cb_8(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).eight();    
+}
+void Checkerboard_window::cb_9(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).nine();    
+}
+void Checkerboard_window::cb_10(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).ten();    
+}
+void Checkerboard_window::cb_11(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).eleven();    
+}
+void Checkerboard_window::cb_12(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).twelve();    
+}
+void Checkerboard_window::cb_13(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).thirteen();    
+}
+void Checkerboard_window::cb_14(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).fourteen();    
+}
+void Checkerboard_window::cb_15(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).fifteen();    
+}
+void Checkerboard_window::cb_16(Address, Address pw)
+{  
+    reference_to<Checkerboard_window>(pw).sixteen();    
+}
+
+//------------------------------------------------------------------------------
+
+void Checkerboard_window::one() //hide two
+{
+    button2.hide();
+}
+
+void Checkerboard_window::two() //hide one
+{
+    button1.hide();
+}
+
+void Checkerboard_window::three() //reset hide and color
+{
+    button1.show();
+    button2.show();
+}
+void Checkerboard_window::four() //turn row into random colors
+{
+    //Author suggests color change but not sure how to achieve this
+}
+
+void Checkerboard_window::five() 
+{
+    button5.move(rand()%x_max(),rand()%y_max());
+}
+
+void Checkerboard_window::six() //reset hide
+{
+    random_box.put(button6.loc.x);
+}
+void Checkerboard_window::seven() //weird pointer thing
+{
+    int abc ;
+    int* ptr = &abc;
+    random_box.put(to_string(ptr));
+}
+
+void Checkerboard_window::eight() 
+{
+    random_box.move(rand()%x_max(),rand()%y_max());
+}
+void Checkerboard_window::nine() //reset hide
+{
+    //do things
+}
+void Checkerboard_window::ten() //hide two
+{
+    //do things
+}
+void Checkerboard_window::eleven() //hide one
+{
+    //do things
+}
+void Checkerboard_window::twelve() //reset hide
+{
+    //do things
+}
+void Checkerboard_window::thirteen() //reset hide
+{
+    //do things
+}
+void Checkerboard_window::fourteen() //hide two
+{
+    //do things
+}
+
+void Checkerboard_window::fifteen() //hide one
+{
+    //do things
+}
+
+void Checkerboard_window::sixteen() //reset hide
+{
+    //do things
+}
+//------------------------------------------------------------------------------
+//EX 02--
--- CHAPTER16/sources.h
+++ CHAPTER16/sources.h
@@ -18,3 +18,60 @@ private:
     void quit();
 };
 //EX 01--
+
+//EX 02
+struct Checkerboard_window : My_window {
+	Checkerboard_window(Point xy, int w, int h, const string& title ); //needs to be 4x4
+private:
+	Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
+    Button button10;
+    Button button11;
+    Button button12;
+    Button button13;
+    Button button14;
+    Button button15;
+    Button button16;
+    Out_box random_box;
+	
+	static void cb_1(Address, Address);
+    static void cb_2(Address, Address);
+    static void cb_3(Address, Address);
+    static void cb_4(Address, Address);
+    static void cb_5(Address, Address);
+    static void cb_6(Address, Address);
+    static void cb_7(Address, Address);
+    static void cb_8(Address, Address);
+    static void cb_9(Address, Address);
+    static void cb_10(Address, Address);
+    static void cb_11(Address, Address);
+    static void cb_12(Address, Address);
+    static void cb_13(Address, Address);
+    static void cb_14(Address, Address);
+    static void cb_15(Address, Address);
+    static void cb_16(Address, Address);
+	void one(); 
+    void two();
+    void three(); 
+    void four();
+    void five(); 
+    void six();
+    void seven(); 
+    void eight();
+    void nine(); 
+    void ten();
+    void eleven(); 
+    void twelve();
+    void thirteen(); 
+    void fourteen();
+    void fifteen(); 
+    void sixteen();
+};
+//EX 02--
