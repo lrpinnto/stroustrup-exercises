89cab9bc9ac9e64f7a3bc8843b6501db14248026
Luis Pinto <luisricpinto@gmail.com>
2021-10-16 16:17:06 +0100

    Solved C16E05.cpp

--- CHAPTER16/C16E05.cpp
+++ CHAPTER16/C16E05.cpp
@@ -2,25 +2,41 @@
 
 #include "./sources.h" 
 
+struct pairs
+{
+    int first() const {return a;}
+    int second() const {return b;}
+    void set_first(int first) {a=first;}
+    void set_second(int second) {b=second;}
+private:
+    int a;
+    int b;
+};
+
+istream& operator>>(istream& is, pairs& p)
+{
+    int a,b;
+    cin>>a>>b;
+    p.set_first(a);
+    p.set_second(b);
+    return is;
+}
+
 int main()
 try
 {
     constexpr int win_x {600};
     constexpr int win_y {400};
-    cout<<"Please input coordinate pairs, separated by space";
-    vector<pair<int,int>> coords;
-    while(true)
-    {
-        pair<int,int> abc;
-        cin>>abc;
-        coords.push_back(abc);
-    }
+    cout<<"Please input coordinate pairs, separated by space\n";
+    vector<pairs> coords;
+    pairs abc;
+    while(cin>>abc) coords.push_back(abc);
     My_window win {Point{100,100},win_x,win_y,"EX 05"};
-    win.wait_for_button();
     Rectangle square {{0,0},40,40}; //random shape
-    for (pair<int,int> p : coords)
+    win.attach(square);
+    for (pairs p : coords)
     {
-        square.move(p.first-square.point(0).x,p.second-square.point(0).y);
+        square.move(p.first()-square.point(0).x,p.second()-square.point(0).y);
         win.wait_for_button();
     }
 }
