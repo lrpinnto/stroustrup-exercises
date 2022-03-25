4f074d5c04d0fa2ae9f84b25f58dc1720d8a9f9e
Luis Pinto <luisricpinto@gmail.com>
2022-03-25 23:43:39 +0000

    Solved C19TRY03

--- CHAPTER19/C19TRY02.cpp
+++ CHAPTER19/C19TRY02.cpp
@@ -2,6 +2,48 @@
 #include "vector.h"
 #include <iostream>
 
+//Vector definition to avoid conflicts with TRY03
+template<typename T, typename A>
+void vector<T,A>::reserve(int newalloc)
+{
+    if(newalloc<=space) return;
+    T* p=alloc.allocate(newalloc);
+    for(int i=0; i<sz; ++i) alloc.construct(&p[i],elem[i]);
+    for(int i=0; i<sz; ++i) alloc.destroy(&elem[i]);
+    alloc.deallocate(elem,space);
+    elem=p;
+    space=newalloc;
+    /*
+    if(newalloc<=this->space) return;
+    vector_base<T,A> b(this->alloc,newalloc);
+    std::unintilized_copy(b.elem,&b.elem[this->sz],this->elem);
+    for (int i = 0; i < this->sz; ++i)
+    {
+        this->alloc.destroy(&this->elem[i]);
+    }
+    std::swap<vector_base<T,A>>(*this,b);
+    */
+}
+
+template<typename T, typename A>
+void vector<T,A>::push_back(const T& val)
+{
+    if(space==0) reserve(8);
+    else if (sz==space) reserve(2*space);
+    alloc.construct(&elem[sz],val);
+    ++sz;
+}
+
+template<typename T, typename A>
+void vector<T,A>::resize(int newsize, T val)
+{
+    reserve(newsize);
+    for(int i=sz; i<newsize; ++i) alloc.construct(&elem[i],val);
+    for(int i=newsize; i<sz; ++i) alloc.destroy(&elem[i]);
+    sz=newsize;
+}
+//------------------------
+
 void suspicious(vector<int>& v, int s)
 {
     int* p = new int[s];
--- CHAPTER19/C19TRY03.cpp
+++ CHAPTER19/C19TRY03.cpp
@@ -1,17 +1,40 @@
 //Chapter 19 try this 03
-//LINK to header vector
 #include <memory>
-
-using namespace std;
+#include "vector.h"
 
 template<typename T, typename A>
 void vector<T,A>::reserve(int newalloc)
 {
     if (newalloc<=space) return;
-    unique_ptr<T> p= alloc.allocate(newalloc)
-    for(int i=0;i<sz;++i) alloc.construct(&p[i],elem[i]);
+    std::unique_ptr<T> p {alloc.allocate(newalloc)};
+    for(int i=0;i<sz;++i) alloc.construct(&(p.get()[i]),elem[i]);
     for(int i=0;i<sz;++i) alloc.destroy(&elem[i]);
     alloc.deallocate(elem,space);
     elem=p.release();
     space=newalloc;
+}
+
+template<typename T, typename A>
+void vector<T,A>::push_back(const T& val)
+{
+    if(space==0) reserve(8);
+    else if (sz==space) reserve(2*space);
+    alloc.construct(&elem[sz],val);
+    ++sz;
+}
+
+template<typename T, typename A>
+void vector<T,A>::resize(int newsize, T val)
+{
+    reserve(newsize);
+    for(int i=sz; i<newsize; ++i) alloc.construct(&elem[i],val);
+    for(int i=newsize; i<sz; ++i) alloc.destroy(&elem[i]);
+    sz=newsize;
+}
+
+int main()
+{
+    vector<int>abc {1,2,3,4};
+    abc.push_back(5);
+    std::cout<<abc[4];
 }
