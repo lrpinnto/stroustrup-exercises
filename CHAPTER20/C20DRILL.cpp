5a7beb2d370f189d2c773732d300d1039e5bc53d
Luis Pinto <luisricpinto@gmail.com>
2022-04-29 18:25:18 +0100

    Added C20DRILL.cpp

---
+++ CHAPTER20/C20DRILL.cpp
@@ -0,0 +1,13 @@
+//just a code test. to delete and replace with actual code
+
+//TO DO
+
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    std::vector<int>a (20);
+    std::cout<<a.capacity();
+    std::cout<<a.at(19);
+}
