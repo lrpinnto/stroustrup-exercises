5ef21a3ff3701e25baf0e307ad2bed9a10ab0e79
Luis Pinto <luisricpinto@gmail.com>
2023-03-05 02:14:02 +0000

    C19E02 adjustments

--- CHAPTER19/C19E02.cpp
+++ CHAPTER19/C19E02.cpp
@@ -4,48 +4,21 @@
 #include <iostream>
 using namespace std;
 
-template<typename T>
-int f(const vector<T>& vt, const vector<T>& vu)
-{
-    vector<T> vect ;
-    vector<T> v ;
-    if(vt.size()>vu.size())
-    {
-        vect =vu;
-        v =vt;
-        vect.resize(vt.size());
-    }
-    else
-    {
-        vect =vt;
-        v =vu;
-        vect.resize(vu.size());
-    }
-    if(v.size()!=vect.size()) throw runtime_error("vectors size() not equal");
+template<typename T, typename U>
+int f(const vector<T>& vt, const vector<U>& vu) //this assumes multiplication with default constructors will always result in 0 (therefore picking the smallest vector)
+{     
     int sum {0};
-    for (int i = 0; i < v.size(); i++)
+    for (int i = 0; i < (vt.size() > vu.size() ? vu.size() : vt.size()) ; i++) //Pick the smallest of the 2
     {
-        sum+=v[i]*vect[i];
+        sum+=vt[i]*vu[i];
     }
     return sum;
-}
-
-template < class T >
-ostream& operator << (ostream& os, const vector<T>& v) //cout behaviour for vector<int> (and any other typename)
-{
-    os << "[";
-    for (typename vector<T>::const_iterator ii = v.begin(); ii != v.end(); ++ii)
-    {
-        os << " " << *ii;
-    }
-    os << "]";
-    return os;
 }
 
 int main()
 try {
     vector<int> a {1,2,3,4,5,6};
-    vector<int> b {1,1,1,1,1}; //if vect size() b < a then f() adds b default constructor
+    vector<char> b {1,1,1,1,1}; //if vect size() b < a then f() adds b default constructor
     cout<<f(a,b);
 }
 catch(std::exception& e) {
