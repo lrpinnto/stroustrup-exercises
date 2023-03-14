f3613a8bbbe544073451ab8238cba2e6beb3cd8e
Luis Pinto <luisricpinto@gmail.com>
2023-03-14 17:01:00 +0000

    Added files

---
+++ CHAPTER20/C20E03.cpp
@@ -0,0 +1,5 @@
+//CHAPTER 20 EX 03
+
+//Look at the palindrome examples (18.7); redo the Jack-and-Jill example from 20.1.2 using that variety of techniques
+
+//skipped
---
+++ CHAPTER20/C20E04.cpp
@@ -0,0 +1,5 @@
+//CHAPTER 20 EX 04
+
+//Find and fix the errors in the Jack-and-Jill example from 20.3.1 by using STL techniques throughout.
+
+//skipped
