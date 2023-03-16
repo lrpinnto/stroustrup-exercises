fd799c566e41f1eaeaf2bf451333d2612cfbf4f5
Luis Pinto <luisricpinto@gmail.com>
2023-03-16 18:04:09 +0000

    Added files

---
+++ CHAPTER21/C21TRY01.cpp
@@ -0,0 +1,6 @@
+//CHAPTER 21 TRY 01
+
+//Are you sure those two definitions are logically equivalent? How would you be sure? Try constructing an argument for their being equivalent.
+//That done, try both on some data. (...)
+
+//Skipped
---
+++ CHAPTER21/C21TRY02.cpp
@@ -0,0 +1,6 @@
+// CHAPTER 21 TRY 02
+
+//Why are we so disgusted with that use of v? Give at least 3 ways this could lead to obscure errors. List 3 applications in which you'd particularly
+//hate to find such code.
+
+//Skipped
