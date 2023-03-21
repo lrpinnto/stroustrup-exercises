98e1f862f39a9f845de0a91f46ccef8cf5258365
Luis Pinto <luisricpinto@gmail.com>
2023-03-21 01:17:59 +0000

    Solved C21E03

--- CHAPTER21/C21E03.cpp
+++ CHAPTER21/C21E03.cpp
@@ -1,3 +1,25 @@
 // CHAPTER 21 EX 03
 
 // Implement count() yourself
+
+#include <vector>
+#include <iostream>
+
+template<typename In, typename T>
+int count(In first, In last, const T& val)
+{
+    int count {0};
+    while(first!=last)
+    {
+        if(*first==val)  count++; 
+        first++;
+    }
+    return count;
+}
+
+int main()
+{
+    std::vector<int> a(10,20);
+    a.insert(a.end()-a.size()/2,5);
+    std::cout<<count(a.begin(),a.end(),20);
+}
