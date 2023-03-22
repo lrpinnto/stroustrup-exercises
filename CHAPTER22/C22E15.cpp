94816df5d9d293166e3b70c9b38e20b09d7c7003
Luis Pinto <luisricpinto@gmail.com>
2023-03-22 14:15:09 +0000

    Added C22 files

---
+++ CHAPTER22/C22E15.cpp
@@ -0,0 +1,3 @@
+// CHAPTER 22 EX 15
+
+// Write a program that, given a file of (name,year) pairs, such as (Algol,1960) and (C,1974), graphs the names on a timeline.
---
+++ CHAPTER22/C22E16.cpp
@@ -0,0 +1,4 @@
+// CHAPTER 22 EX 16
+
+// Modify the program from the previous exercise so that it reads a file of (name,year,(ancestors)) tuples, such as (Fortran,1956,()), 
+// (Algol,1960,(Fortran)), and (C++,1985,(C,Simula)), and graphs them on a timeline with arrows forma ancestors to descendants. 
